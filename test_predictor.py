a14da701e7b5464b636ddf123765617d899bc0eb
Marek <zylamarek@gmail.com>
2019-10-16 14:53:23 +0200

    visualize predicted landmarks

--- test_predictor.py
+++ test_predictor.py
@@ -1,3 +1,5 @@
+import cv2
+import os
 import numpy as np
 from keras.models import load_model
 from collections import defaultdict
@@ -5,8 +7,11 @@ from collections import defaultdict
 dirname = 'test'
 bbs_model_name = '2019_10_14_22_16_22'
 lmks_model_name = '2019_10_14_23_50_08'
+base_path = '../cat-dataset/data/clean/%s' % dirname
 
 img_size = 224
+file_list = [f for f in sorted(os.listdir(base_path)) if f.endswith('.cat')]
+os.makedirs('output', exist_ok=True)
 
 data_lmks = np.load('dataset/lmks_pred_%s_%s.npy' % (bbs_model_name, dirname), allow_pickle=True)
 x_lmks = np.array(data_lmks.item().get('imgs')).astype('float32') / 255.
@@ -20,8 +25,8 @@ predicted_lmks = lmks_model.predict(x_lmks, verbose=1)
 mse_eval = lmks_model.evaluate(x_lmks, y_lmks, verbose=1)
 
 mses = defaultdict(list)
-for landmarks_predicted, lmks_transform, landmarks_original, img_max_size in \
-        zip(predicted_lmks, lmks_transforms, landmarks_originals, img_max_sizes):
+for landmarks_predicted, lmks_transform, landmarks_original, img_max_size, filename in \
+        zip(predicted_lmks, lmks_transforms, landmarks_originals, img_max_sizes, file_list):
     landmarks_predicted = landmarks_predicted.reshape((-1, 2)) - np.array(lmks_transform[1:3])
     landmarks_predicted /= lmks_transform[0]
     landmarks_predicted += lmks_transform[3]
@@ -39,6 +44,13 @@ for landmarks_predicted, lmks_transform, landmarks_original, img_max_size in \
     mses['eyes normalized'].append(get_mse_normalized(0, 1))
     mses['mouth normalized'].append(get_mse_normalized(2, 2))
     mses['ears normalized'].append(get_mse_normalized(3, 8))
+
+    img = cv2.imread(os.path.join(base_path, filename[:-4]))
+    for i_l, (l_p, l_o) in enumerate(zip(landmarks_predicted.astype('int'), landmarks_original)):
+        cv2.circle(img, center=tuple(l_p), radius=1, color=(255, 0, 0), thickness=2)
+        cv2.putText(img, str(i_l), tuple(l_p), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        cv2.circle(img, center=tuple(l_o), radius=1, color=(0, 255, 0), thickness=2)
+    cv2.imwrite('output/%.5f.png' % mse, img)
 
 print('mse all: %.7f' % np.mean(mses['all']))
 for name, vals in mses.items():
