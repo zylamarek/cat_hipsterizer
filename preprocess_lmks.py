1f5dc32c61149efaa377dadf3deb4bf168d5cd76
Marek <zylamarek@gmail.com>
2019-10-21 09:30:54 +0200

    proper landmarks testing

--- preprocess_lmks.py
+++ preprocess_lmks.py
@@ -1,21 +1,19 @@
-import random
 import sys
 import cv2
 import os
 import pandas as pd
 import numpy as np
 
-dirname = 'test'
+dirname = 'landmarks_test'
 base_path = '../cat-dataset/data/clean/%s' % dirname
+crop = False
 
 img_size = 224
 file_list = sorted(os.listdir(base_path))
-random.shuffle(file_list)
 
 dataset = {
     'imgs': [],
-    'lmks': [],
-    'bbs': []
+    'lmks': []
 }
 
 
@@ -41,22 +39,26 @@ for f in file_list:
     # read landmarks
     pd_frame = pd.read_csv(os.path.join(base_path, f), sep=' ', header=None)
     landmarks = (pd_frame.as_matrix()[0][1:((pd_frame.shape[1] - 1) // 2) * 2 + 1]).reshape((-1, 2))
-    bb = np.array([np.min(landmarks, axis=0), np.max(landmarks, axis=0)]).astype(np.int)
-    center = np.mean(bb, axis=0)
-    face_size = max(np.abs(np.max(landmarks, axis=0) - np.min(landmarks, axis=0)))
-    new_bb = np.array([
-        center - face_size * 0.6,
-        center + face_size * 0.6
-    ]).astype(np.int)
-    new_bb = np.clip(new_bb, 0, 99999)
-    new_landmarks = landmarks - new_bb[0]
 
     # load image
     img_filename, ext = os.path.splitext(f)
 
     img = cv2.imread(os.path.join(base_path, img_filename))
 
-    new_img = img[new_bb[0][1]:new_bb[1][1], new_bb[0][0]:new_bb[1][0]]
+    if crop:
+        bb = np.array([np.min(landmarks, axis=0), np.max(landmarks, axis=0)]).astype(np.int)
+        center = np.mean(bb, axis=0)
+        face_size = max(np.abs(np.max(landmarks, axis=0) - np.min(landmarks, axis=0)))
+        new_bb = np.array([
+            center - face_size * 0.6,
+            center + face_size * 0.6
+        ]).astype(np.int)
+        new_bb = np.clip(new_bb, 0, 99999)
+        new_landmarks = landmarks - new_bb[0]
+        new_img = img[new_bb[0][1]:new_bb[1][1], new_bb[0][0]:new_bb[1][0]]
+    else:
+        new_img = img.copy()
+        new_landmarks = landmarks.copy()
 
     # resize image and relocate landmarks
     img, ratio, top, left = resize_img(new_img)
@@ -64,7 +66,6 @@ for f in file_list:
 
     dataset['imgs'].append(img)
     dataset['lmks'].append(new_landmarks.flatten())
-    dataset['bbs'].append(new_bb.flatten())
 
     # for l in new_landmarks:
     #   cv2.circle(img, center=tuple(l), radius=1, color=(255, 255, 255), thickness=2)
--- test_separately.py
+++ test_separately.py
@@ -3,6 +3,7 @@ from keras.models import load_model
 import keras.backend as K
 
 dirname = 'test'
+dirname_landmarks = 'landmarks_test'
 bbs_model_name = '2019_10_14_22_16_22'
 lmks_model_name = '2019_10_14_23_50_08'
 img_size = 224
@@ -11,7 +12,7 @@ data_bbs = np.load('dataset/%s.npy' % dirname, allow_pickle=True)
 x_bbs = np.array(data_bbs.item().get('imgs')).astype('float32') / 255.
 y_bbs = np.array(data_bbs.item().get('bbs'))
 
-data_lmks = np.load('dataset/lmks_%s.npy' % dirname, allow_pickle=True)
+data_lmks = np.load('dataset/lmks_%s.npy' % dirname_landmarks, allow_pickle=True)
 x_lmks = np.array(data_lmks.item().get('imgs')).astype('float32') / 255.
 y_lmks = np.array(data_lmks.item().get('lmks'))
 
@@ -35,16 +36,19 @@ def iou_metric(y_true, y_pred):
     return K.mean(iou_, axis=-1)
 
 
-lmks_model = load_model('models/%s.h5' % lmks_model_name)
-lmks_mse = lmks_model.evaluate(x_lmks, y_lmks)
-
 bbs_model = load_model('models/%s.h5' % bbs_model_name)
-bbs_model.compile(optimizer='sgd', loss='mse', metrics=[iou_metric])
-bbs_mse, bbs_iou = bbs_model.evaluate(x_bbs, y_bbs)
+bbs_model.compile(optimizer='sgd', loss='mse', metrics=['mae', iou_metric])
+bbs_mse, bbs_mae, bbs_iou = bbs_model.evaluate(x_bbs, y_bbs)
 
-print('bbs MSE:', bbs_mse)
-print('bbs RMSE:', np.sqrt(bbs_mse))
-print('bbs IoU:', np.sqrt(bbs_iou))
+lmks_model = load_model('models/%s.h5' % lmks_model_name)
+lmks_model.compile(optimizer='sgd', loss='mse', metrics=['mae'])
+lmks_mse, lmks_mae = lmks_model.evaluate(x_lmks, y_lmks)
 
-print('lmks MSE:', lmks_mse)
-print('lmks RMSE:', np.sqrt(lmks_mse))
+print('bbs IoU:\t%.2f' % (bbs_iou * 100.))
+print('bbs MAE:\t%.2f' % bbs_mae)
+print('bbs MSE:\t%.2f' % bbs_mse)
+print('bbs RMSE:\t%.2f' % np.sqrt(bbs_mse))
+
+print('lmks MAE:\t%.2f' % lmks_mae)
+print('lmks MSE:\t%.2f' % lmks_mse)
+print('lmks RMSE:\t%.2f' % np.sqrt(lmks_mse))
